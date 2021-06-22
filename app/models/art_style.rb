cff8963601cf40c7d7ff352b2e01864d2e52e8d7
Pedro Venzi <pvenzi.pv@gmail.com>
2021-06-22 16:07:17 -0300

    :sparkles: Model ArtStyle, Configuracao RSpec/FactoryBot

--- app/models/art_style.rb
+++ app/models/art_style.rb
@@ -1,3 +1,5 @@
 class ArtStyle < ApplicationRecord
-  belongs_to :paintings
+  validates :name, :description, presence: true
+  validates_length_of :name, minimum: 3
+  validates_length_of :description, maximum: 200
 end
--- db/migrate/20210622183758_create_art_styles.rb
+++ db/migrate/20210622183758_create_art_styles.rb
@@ -2,7 +2,6 @@ class CreateArtStyles < ActiveRecord::Migration[5.2]
   def change
     create_table :art_styles do |t|
       t.string :name
-      t.references :paintings, foreign_key: true
 
       t.timestamps
     end
